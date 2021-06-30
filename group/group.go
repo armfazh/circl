79c84cab04643f27921f616f6b1829bee60095ce
armfazh <armfazh@cloudflare.com>
2021-06-29 18:35:34 -0700

    HashToGroup uses io.Write API for consuming input.

--- group/group.go
+++ group/group.go
@@ -7,6 +7,7 @@ import (
 	"io"
 )
 
+// Params denotes the length in bytes of elements and scalar of a group.
 type Params struct {
 	ElementLength           uint // Length in bytes of an element.
 	CompressedElementLength uint // Length in bytes of a compressed element.
@@ -23,9 +24,9 @@ type Group interface {
 	Order() Scalar
 	RandomElement(io.Reader) Element
 	RandomScalar(io.Reader) Scalar
-	HashToElement(data, dst []byte) Element
-	HashToElementNonUniform(b, dst []byte) Element
-	HashToScalar(data, dst []byte) Scalar
+	NewHash(dst []byte) HashToElement
+	NewHashNonUniform(dst []byte) HashToElement
+	NewHashToScalar(dst []byte) HashToScalar
 }
 
 // Element represents an abstract element of a prime-order group.
@@ -52,6 +53,30 @@ type Scalar interface {
 	Inv(Scalar) Scalar
 	encoding.BinaryMarshaler
 	encoding.BinaryUnmarshaler
+}
+
+// HashToElement allows to hash a slice of bytes to produce an Element of a group.
+type HashToElement interface {
+	// Reset cleans the internal state to allow writing another input. Discards
+	// any input written previously.
+	Reset()
+	// Use writer to consume the input of the hash. Panics if it is invoked
+	// after Sum function was called.
+	io.Writer
+	// Sum returns the result of the hash.
+	Sum() Element
+}
+
+// HashToScalar allows to hash a slice of bytes to produce a Scalar of a group.
+type HashToScalar interface {
+	// Reset cleans the internal state to allow writing another input. Discards
+	// any input written previously.
+	Reset()
+	// Use writer to consume the input of the hash. Panics if it is invoked
+	// after Sum function was called.
+	io.Writer
+	// Sum returns the result of the hash.
+	Sum() Scalar
 }
 
 var (
