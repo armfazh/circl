8fba75ffe8d96f45d35f84999c57e5cf15c250e0
armfazh <armfazh@cloudflare.com>
2022-07-11 07:50:24 -0700

    Working on signature.

--- tss/frost/commit.go
+++ tss/frost/commit.go
@@ -2,19 +2,25 @@ package frost
 
 import (
 	"encoding/binary"
+	"errors"
 	"sort"
 
 	"github.com/cloudflare/circl/group"
 )
 
-type commitment struct {
-	id              uint16
+type Nonce struct {
+	Id              uint16
+	hiding, binding group.Scalar
+}
+
+type Commitment struct {
+	Id              uint16
 	hiding, binding group.Element
 }
 
-func (c commitment) MarshalBinary() ([]byte, error) {
+func (c Commitment) MarshalBinary() ([]byte, error) {
 	bytes := (&[2]byte{})[:]
-	binary.BigEndian.PutUint16(bytes, c.id)
+	binary.BigEndian.PutUint16(bytes, c.Id)
 
 	h, err := c.hiding.MarshalBinary()
 	if err != nil {
@@ -28,7 +34,26 @@ func (c commitment) MarshalBinary() ([]byte, error) {
 	return append(append(bytes, h...), b...), nil
 }
 
-func bindingFactor(commitEncoded []byte, msg []byte) group.Scalar {
+func isSorted(c []Commitment) error {
+	if !sort.SliceIsSorted(c, func(i, j int) bool { return c[i].Id < c[j].Id }) {
+		return errors.New("frost: commitments must be sorted")
+	}
+	return nil
+}
+
+func encodeComs(coms []Commitment) ([]byte, error) {
+	var out []byte
+	for i := range coms {
+		cEnc, err := coms[i].MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, cEnc...)
+	}
+	return out, nil
+}
+
+func calcBindingFactor(commitEncoded []byte, msg []byte) group.Scalar {
 	msgHash := h3(msg)
 	rho := append(append([]byte{}, commitEncoded...), msgHash...)
 	return h1(rho)
@@ -36,11 +61,7 @@ func bindingFactor(commitEncoded []byte, msg []byte) group.Scalar {
 
 type groupCommitment struct{ gc group.Element }
 
-func getGroupCommitment(g group.Group, c []commitment, bf group.Scalar) groupCommitment {
-	if !sort.SliceIsSorted(c, func(i, j int) bool { return c[i].id < c[j].id }) {
-		panic("commitments must be sorted")
-	}
-
+func calcGroupCommitment(g group.Group, c []Commitment, bf group.Scalar) groupCommitment {
 	gh := g.NewElement()
 	gb := g.NewElement()
 
--- tss/frost/frost.go
+++ tss/frost/frost.go
@@ -2,6 +2,7 @@ package frost
 
 import (
 	"io"
+	"sort"
 
 	"github.com/cloudflare/circl/group"
 )
@@ -11,6 +12,36 @@ type PublicKey group.Element
 type Peer struct {
 	Id       uint16
 	keyShare group.Scalar
+	g        group.Group
+}
+
+func (p Peer) Commit(rnd io.Reader) (Nonce, Commitment) {
+	hidingNonce := nonceGenerate(rnd, p.keyShare)
+	bindingNonce := nonceGenerate(rnd, p.keyShare)
+
+	hidingNonceCom := p.g.NewElement().MulGen(hidingNonce)
+	bindingNonceCom := p.g.NewElement().MulGen(bindingNonce)
+	return Nonce{p.Id, hidingNonce, bindingNonce}, Commitment{p.Id, hidingNonceCom, bindingNonceCom}
+}
+
+func (p Peer) ShareSign(msg []byte, pubKey PublicKey, nonce Nonce, coms []Commitment) ([]byte, error) {
+	if p.Id != nonce.Id {
+		panic("frost: bad id")
+	}
+
+	if !sort.SliceIsSorted(coms, func(i, j int) bool { return coms[i].Id < coms[j].Id }) {
+		panic("Commitments must be sorted")
+	}
+
+	comsEnc, err := encodeComs(coms)
+	if err != nil {
+		return nil, err
+	}
+	bindingFactor := calcBindingFactor(comsEnc, msg)
+	groupCom := calcGroupCommitment(p.g, coms, bindingFactor)
+
+	return nil, nil
+
 }
 
 func h1(m []byte) group.Scalar { return nil }
@@ -18,7 +49,7 @@ func h3(m []byte) []byte       { return nil }
 func h2(m []byte) group.Scalar { return nil }
 func h4(m []byte) group.Scalar { return nil }
 
-func nonceGenerator(rnd io.Reader, s group.Scalar) group.Scalar {
+func nonceGenerate(rnd io.Reader, s group.Scalar) group.Scalar {
 	k := make([]byte, 32)
 	_, _ = io.ReadFull(rnd, k)
 	secretEnc, _ := s.MarshalBinary()
